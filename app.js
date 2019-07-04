dc01f03e82db0c940faaf92dac4b6cefc2f0c37d
unknown <manusharma8826@gmail.com>
2019-07-05 04:09:44 +0530

    updated

--- app.js
+++ app.js
@@ -1,24 +1,118 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
 const app = express();
 
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//method override 
+app.use(methodOverride('_method'));
+
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://manusharma99:manu@9990738223@cluster0-ta5ng.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
+.then(() => console.log('mongodb is connected'))
+.catch(err => console.log(err));
+
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+//handlebars
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
  
-// index route
 
+//routes
 app.get('/', (req,res) => {
     const title = 'WELCOME';
- res.render('index' , {title:title});
+ res.render('home' , {title:title});
 });
 
+//About
 app.get('/about', (req,res) => {
     res.render('about');
    });
+//add ideas
+app.get('/ideas/add',(req,res) =>{
+ res.render('ideas/add');
+});
+//edit ideas
+app.get('/ideas/edit/:id',(req,res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then( idea => {
+        res.render('ideas/edit',
+        {idea:idea});
+    });
+});
 
+//index route
+app.get('/ideas',(req,res)=>{
+    Idea.find({})
+    .sort({date:'desc'})
+    .then(ideas=>{
+        res.render('ideas/index',{ideas:ideas})
+        
+    }
+    )
+});
+
+app.post('/ideas', (req,res)=>{
+    let errors = [];
+    if(!req.body.title)
+    {
+      errors.push({text:'please fill the title'});
+    }
+    if (!req.body.details)
+    {
+        errors.push({text:'please fill the details'});
+    }
+    if(errors.length > 0)
+    {
+        res.render('ideas/add',{
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+     }
+    else 
+    {
+        const newUser = {
+            title:req.body.title,
+            details:req.body.details
+        }
+    new Idea(newUser)
+    .save()
+    .then(idea =>{
+        res.redirect('/ideas');
+    })
+    }
+});
+
+//put 
+app.put('/ideas/:id',(req,res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea=>{
+        idea.title=req.body.title;
+        idea.details=req.body.details;
+        idea.save()
+        .then(idea=>{
+            res.redirect('/ideas');
+        })
+
+    })
+    
+})
 const port = 5000;
 app.listen(port, () => {
     console.log('server started on port ' + port);
-});
+}); 
---
+++ models/Idea.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var SomeModelSchema = new Schema({
+  title:{
+    type: String,
+    required:true
+  },
+  details:{
+      type: String,
+      required:true
+  },
+  date:{ 
+      type:Date,
+      default:Date.now
+  }
+});
+
+mongoose.model('ideas',SomeModelSchema);
