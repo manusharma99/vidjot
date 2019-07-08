42893d592891578a8c7da01881e18ab2c2d395d9
unknown <manusharma8826@gmail.com>
2019-07-09 02:42:12 +0530

    updated

--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// const passport = require('passport');
 const router = express.Router();
 
 module.exports =router;
@@ -12,3 +14,63 @@ router.get('/register',(req,res)=>{
        res.render('users/register');
 });
 
+//user model load
+require('../models/User');
+const User = mongoose.model('users');
+
+//register post form 
+
+router.post('/register',(req,res)=>{
+let errors = [];
+
+if(req.body.password != req.body.password2){
+    errors.push({text:'Password does not match'});
+}
+if (req.body.password.length < 4){
+    errors.push({text:'password too small'});
+}
+if (errors.length>0){
+    res.render('users/register',{
+        errors:errors,
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        password2:req.body.password2
+     });
+}
+else{
+    User.findOne({email:req.body.email})
+    .then(user=>{
+        if(user)
+      {
+        req.flash('error_msg','Email is already registered');
+        res.redirect('/users/register');
+      }
+      else{
+        const newUser = new User ({
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            password2:req.body.password2
+        })
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(newUser.password,salt,(err,hash)=>{
+            if (err) throw err;
+              newUser.password=hash;
+              newUser.save()
+              .then(user => {
+                  req.flash('success_msg', 'You are now registerd');
+                  res.redirect('/users/login')
+              })
+              .catch(err=>{
+                  console.log(err);
+                  return;
+              })
+    
+            });
+        });
+      }
+    });
+    
+}
+});
