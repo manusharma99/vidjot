cb1e4ff8b636cb9f53993e2ac9e4ef494505a4dc
unknown <manusharma8826@gmail.com>
2019-07-11 21:18:27 +0530

    updated

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(session({
   saveUninitialized: true
 }));
 
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 //body parser
@@ -31,6 +35,7 @@ app.use(function(req,res,next){
     res.locals.success_msg =  req.flash('success_msg');
     res.locals.error_msg =  req.flash('error_msg');
     res.locals.errors = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
--- config/passport.js
+++ config/passport.js
@@ -6,6 +6,35 @@ const User = mongoose.model('users');
 module.exports = function(passport){
     passport.use(new LocalStrategy({usernameField: 'email'},(email,password,done)=>
     {
-        console.log(password);
+     User.findOne({
+         email:email
+     }).then(user=>{
+         if(!user){
+             return done(null,false,{message:'User not found'});
+             console.log(req.body);
+            }
+            else{
+                bcrypt.compare(password,user.password,(err,match)=>{
+                    if (err) throw err;
+                    if (match){
+                        return done(null,user);
+                    }
+                    else{
+                        return done(null,false,{message:'password incorrect'});
+                    }
+                })
+            }
+
+     })
     }));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      });
+      
 }
