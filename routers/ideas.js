4f641163ed4721c4f9b74ce89bf44d81eb9812a2
unknown <manusharma8826@gmail.com>
2019-07-12 00:07:00 +0530

    updated

--- routers/ideas.js
+++ routers/ideas.js
@@ -1,29 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
 
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
 module.exports = router;
 
 //add ideas
-router.get('/add',(req,res) =>{
+router.get('/add',ensureAuthenticated,(req,res) =>{
     res.render('ideas/add');
 });
 //edit ideas
-router.get('/edit/:id',(req,res) => {
+router.get('/edit/:id',ensureAuthenticated,(req,res) => {
     Idea.findOne({
         _id: req.params.id
     })
     .then( idea => {
-        res.render('ideas/edit',
+        if(idea.user != req.user.id){
+            req.flash('error_msg','Not Authorized');
+            res.redirect('/ideas');
+        }
+        else{
+            res.render('ideas/edit',
         {idea:idea});
+        }
     });
 });
 
 //index route
-router.get('/',(req,res)=>{
-    Idea.find({})
+router.get('/',ensureAuthenticated,(req,res)=>{
+    Idea.find({user : req.user.id})
     .sort({date:'desc'})
     .then(ideas=>{
         res.render('ideas/index',{ideas:ideas})
@@ -32,7 +39,7 @@ router.get('/',(req,res)=>{
     )
 });
 
-router.post('/', (req,res)=>{
+router.post('/', ensureAuthenticated,(req,res)=>{
     let errors = [];
     if(!req.body.title)
     {
@@ -54,7 +61,8 @@ router.post('/', (req,res)=>{
     {
         const newUser = {
             title:req.body.title,
-            details:req.body.details
+            details:req.body.details,
+           user:req.user.id
         }
     new Idea(newUser)
     .save()
@@ -83,7 +91,7 @@ router.put('/:id',(req,res) => {
     
 });
 //delete request
-router.delete('/:id',(req,res)=>{
+router.delete('/:id',ensureAuthenticated,(req,res)=>{
    Idea.remove({_id: req.params.id})
    .then(()=>{
        req.flash('success_msg','Idea is removed');
--- routers/users.js
+++ routers/users.js
@@ -81,3 +81,9 @@ else{
     
 }
 });
+
+router.get('/logout',(req,res)=>{
+    req.logout();
+    req.flash('success_msg','You are logged out');
+    res.redirect('/users/login');
+});
